55d3d1e78f14e546940adb30e7689d4478d209cf
Joseph Klinger <klingerj@seas.upenn.edu>
2018-10-30 15:44:02 -0400

    WE CARE

--- GameEngine/src/rendering/VulkanRenderer.cpp
+++ GameEngine/src/rendering/VulkanRenderer.cpp
@@ -426,7 +426,7 @@ void VulkanRenderer::CreateShadowRenderPass() {
     depthAttachment.format = FindDepthFormat(physicalDevice);
     depthAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
     depthAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-    depthAttachment.storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    depthAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
     depthAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
     depthAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
     depthAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
