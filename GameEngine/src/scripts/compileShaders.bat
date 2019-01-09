129923f1549e88f00d7dada018d72ae75d5a6079
Joseph Klinger <klingerj@seas.upenn.edu>
2019-01-09 17:03:48 -0500

    update shader compiler script to use relative path

--- GameEngine/src/scripts/compileShaders.bat
+++ GameEngine/src/scripts/compileShaders.bat
@@ -1,5 +1,5 @@
 echo Compiling Shaders...
 cd src\shaders\
-for %%i in (*.vert) do C:/VulkanSDK/1.1.82.1/Bin32/glslangValidator.exe -V %%i -o %%~ni.spv
-for %%i in (*.frag) do C:/VulkanSDK/1.1.82.1/Bin32/glslangValidator.exe -V %%i -o %%~ni.spv
+for %%i in (*.vert) do ..\..\..\Libraries\VulkanSDK\1.1.82.1\Bin32\glslangValidator.exe -V %%i -o %%~ni.spv
+for %%i in (*.frag) do ..\..\..\Libraries\VulkanSDK\1.1.82.1\Bin32\glslangValidator.exe -V %%i -o %%~ni.spv
 echo Done.
